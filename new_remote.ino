b629d66b0f8c53d125878045af85e10809dab424
jusbro <97056196+jusbro@users.noreply.github.com>
2024-01-21 15:25:40 -0500

    Update new_remote.ino

--- new_remote.ino
+++ new_remote.ino
@@ -1,31 +1,67 @@
+/*
+REMOTE-SIDE WIRELESS TRANSMITTER
 
+This code runs on all of the six wireless remotes. The remotes transmit data to a "whose first" tower. Each remote is given a unique identifier.
+The whose first tower registers remotes that send their identifier and list them in order of acceptance
+
+TO DO LIST:
+1) Add button that will be pressed to send the payload
+  a) Ensure only one transmission of payload per button push
+2) Determine what the final payload will be for each remote
+3) Test on external power
+4) Test without serial connection
+5) Test on Nano Every board (final deployment hardware)
+6) Design and build PCB
+7) Determine if anti-ID is needed in final design of whose first 
+*/
 #include <SPI.h>
 #include "RF24.h"
 
+//Arduino Uno CE 8, CSN 10
+//Arduino Nano CE 7, CSN 8
 #define CE_PIN 7
 #define CSN_PIN 8
-#define SIZE 32             // this is the maximum for this example. (minimum is 1)
 
+// instantiate an object for the nRF24L01 transceiver
 RF24 radio(CE_PIN, CSN_PIN);
 
+//Create this remote's unique ID.
+//This is temporarily A, B, C, etc. but may be changed to any character in the future
+//May also create an "anti-character" ID in the future that sends an "off" command to the whose first tower
+char payload[] = "A";
+
+
 void setup() {
-  Serial.begin(115200);
+  //Start Serial for debug purposes. This can be removed in final product
+  Serial.begin(9600);
+  //Test to see if the radio device is connected properly and has started
   if (!radio.begin()) {
     Serial.println(F("radio hardware is not responding!!"));
-    while (1) {} 
+    while (1) {}  // If not, hold in infinite loop
   }
-  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
-  radio.stopListening();  // put radio in TX mode
-} 
+  //Assign an address to the receiver. MUST MATCH THE TOWER(receiver)
+  radio.openWritingPipe(0xDEADBEEF);  
+
+  //Make sure this device is not listening for broacastes
+  radio.stopListening();
+
+  //Set the power of the transmitter
+  //Settings too high will not work in close prox. to the receiver
+  radio.setPALevel(RF24_PA_LOW);
+
+  //Declare end of startup over serial
+  Serial.println("Radio Started");
+}
 
 void loop() {
-  Serial.println("Transmitting A");
-  radio.writeFast("A", SIZE);
-  delay(100);  // slow transmissions down by 1 second
-  Serial.println("Transmitting B");
-  radio.writeFast("B", SIZE);
-  delay(100);
-  
+  //Send the payload, and verify that it was sent
+  if (radio.writeFast(&payload, sizeof(payload))) {
+    Serial.print(payload);
+    Serial.println(" Payload Successfully Sent");
+} else { //If transmission was not successful..
+    Serial.println("Payload Send failed");
+}
 
-} 
-
+  //temporary delay for debug purposes
+  delay(1000);
+}
